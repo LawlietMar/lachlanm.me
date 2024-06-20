931e9211a1e0a3d5fa2ffae089658a0a314ef2dd
Lachlan <lachlan.marvit@gmail.com>
2024-06-20 14:36:50 -0700

    added side-room

---
+++ pixel/side-room/audio.js
@@ -0,0 +1,31 @@
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    sleep(10).then(() => ready());
+}
+
+var time
+function ready(){
+    var permission = localStorage.getItem('audioAutoplayPermission') === 'granted';
+    time = localStorage.getItem("aud-spot-beach");
+    var aud = document.getElementsByClassName("aud")[0];
+
+    if (permission){
+        aud.currentTime = parseInt(time);
+        aud.play();
+    }
+    incTime(time);
+}
+
+function incTime(){
+    time = parseInt(time) + 1;
+    if (parseInt(time) > 284){
+        time = 0;
+    }
+    localStorage.setItem("aud-spot-beach", time);
+    sleep(1000).then(() => {incTime()});
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
