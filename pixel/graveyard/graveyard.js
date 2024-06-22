60becb423ae3651edcfa8344bfc4cfaea70dc983
Lachlan <lachlan.marvit@gmail.com>
2024-06-21 23:34:47 -0700

    moved test

--- pixel/graveyard/graveyard.js
+++ pixel/graveyard/graveyard.js
@@ -11,6 +11,9 @@ function ready() {
     // Initialize selected item to -1 (no selection)
     localStorage.setItem("selected", -1);
     localStorage.setItem("free", 1);
+
+    localStorage.setItem("has-axe", "true");
+    localStorage.setItem("spot1", "axe");
     
     checkHave();
     
