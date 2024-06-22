9ecf96df5b62c8c9f0a5c75f3e8455a0980b9730
Lachlan <lachlan.marvit@gmail.com>
2024-06-21 23:34:37 -0700

    test items

--- pixel/island-beach/island-beach.js
+++ pixel/island-beach/island-beach.js
@@ -12,8 +12,8 @@ function ready() {
     localStorage.setItem("selected", -1);
     localStorage.setItem("free", 1);
 
-    localStorage.setItem("spot3", "can");
-    localStorage.setItem("spot4", "daisy-seeds");
+    localStorage.setItem("has-axe", "true");
+    localStorage.setItem("spot1", "axe");
     
     checkHave();
     
