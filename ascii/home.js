e18e14b133a847320fb3bf9bc7a399a29296e298
Lachlan <lachlan.marvit@gmail.com>
2024-06-30 21:00:40 -0700

    fixed home

--- ascii/home.js
+++ ascii/home.js
@@ -80,7 +80,7 @@ function resett(ite){
 
     localStorage.setItem("faded", "false");
     localStorage.setItem("sane-bum", "false");
-    localStorage.setItem("fed-bum");
+    localStorage.setItem("fed-bum", "false");
 
     localStorage.setItem("dead-bird", "false");
     localStorage.setItem("dead-birdB", "false");
